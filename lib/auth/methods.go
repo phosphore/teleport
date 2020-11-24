trustex/teleport-1

Database access

lib/auth/methods.go
@@ -227,6 +227,8 @@ type AuthenticateSSHRequest struct {
 	// KubernetesCluster sets the target kubernetes cluster for the TLS
 	// certificate. This can be empty on older clients.
 	KubernetesCluster string `json:"kubernetes_cluster"`
+	// RouteToDatabase specifies name of the database to route requests to.
+	RouteToDatabase string `json:"route_to_database,omitempty"`
 }
 
 // CheckAndSetDefaults checks and sets default certificate values
@@ -352,6 +354,7 @@ func (s *Server) AuthenticateSSHUser(req AuthenticateSSHRequest) (*SSHLoginRespo
 		traits:            user.GetTraits(),
 		routeToCluster:    req.RouteToCluster,
 		kubernetesCluster: req.KubernetesCluster,
+		routeToDatabase:   req.RouteToDatabase,
 	})
 	if err != nil {
 		return nil, trace.Wrap(err)

tool/tctl/common/db_command.go
@@ -1,0 +1,106 @@
+/*
+Copyright 2020 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package common
+
+import (
+	"context"
+	"os"
+
+	"github.com/gravitational/teleport"
+	"github.com/gravitational/teleport/lib/auth"
+	"github.com/gravitational/teleport/lib/defaults"
+	"github.com/gravitational/teleport/lib/service"
+	"github.com/gravitational/teleport/lib/services"
+
+	"github.com/gravitational/kingpin"
+	"github.com/gravitational/trace"
+)
+
+// DBCommand implements "tctl db" group of commands.
+type DBCommand struct {
+	config *service.Config
+
+	// format is the output format (text, json, or yaml).
+	format string
+
+	// dbList implements the "tctl db ls" subcommand.
+	dbList *kingpin.CmdClause
+}
+
+// Initialize allows DBCommand to plug itself into the CLI parser.
+func (c *DBCommand) Initialize(app *kingpin.Application, config *service.Config) {
+	c.config = config
+
+	db := app.Command("db", "Operate on databases registered with the cluster.")
+	c.dbList = db.Command("ls", "List all databases registered with the cluster.")
+	c.dbList.Flag("format", "Output format, 'text', 'json', or 'yaml'").Default("text").StringVar(&c.format)
+}
+
+// TryRun attempts to run subcommands like "apps ls".
+func (c *DBCommand) TryRun(cmd string, client auth.ClientI) (match bool, err error) {
+	switch cmd {
+	case c.dbList.FullCommand():
+		err = c.ListDatabases(client)
+	default:
+		return false, nil
+	}
+	return true, trace.Wrap(err)
+}
+
+// ListDatabases prints the list of database proxies that have recently sent
+// heartbeats to the cluster.
+func (c *DBCommand) ListDatabases(client auth.ClientI) error {
+	servers, err := client.GetDatabaseServers(context.TODO(), defaults.Namespace, services.SkipValidation())
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	coll := &dbCollection{servers: servers}
+	switch c.format {
+	case teleport.Text:
+		err = coll.writeText(os.Stdout)
+	case teleport.JSON:
+		err = coll.writeJSON(os.Stdout)
+	case teleport.YAML:
+		err = coll.writeYAML(os.Stdout)
+	default:
+		return trace.BadParameter("unknown format %q", c.format)
+	}
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return nil
+}
+
+const dbMessage = `The invite token: %v
+This token will expire in %d minutes
+
+Fill out and run this command on a node to make the application available:
+
+> teleport start \
+   --roles=%v \
+   --token=%v \
+   --ca-pin=%v \
+   --auth-server=%v \
+   --db-name=%v \
+   --db-protocol=%v \
+   --db-address=%v 
+
+Please note:
+
+  - This invitation token will expire in %d minutes
+  - %v must be reachable from the new database service
+`

phosphore/teleport

lib/auth/methods.go
@@ -227,6 +227,8 @@ type AuthenticateSSHRequest struct {
 	// KubernetesCluster sets the target kubernetes cluster for the TLS
 	// certificate. This can be empty on older clients.
 	KubernetesCluster string `json:"kubernetes_cluster"`
+	// RouteToDatabase specifies name of the database to route requests to.
+	RouteToDatabase string `json:"route_to_database,omitempty"`
 }
 
 // CheckAndSetDefaults checks and sets default certificate values
@@ -352,6 +354,7 @@ func (s *Server) AuthenticateSSHUser(req AuthenticateSSHRequest) (*SSHLoginRespo
 		traits:            user.GetTraits(),
 		routeToCluster:    req.RouteToCluster,
 		kubernetesCluster: req.KubernetesCluster,
+		dbName:            req.RouteToDatabase,
 	})
 	if err != nil {
 		return nil, trace.Wrap(err)

tool/tctl/common/db_command.go
@@ -1,0 +1,106 @@
+/*
+Copyright 2020 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package common
+
+import (
+	"context"
+	"os"
+
+	"github.com/gravitational/teleport"
+	"github.com/gravitational/teleport/lib/auth"
+	"github.com/gravitational/teleport/lib/defaults"
+	"github.com/gravitational/teleport/lib/service"
+	"github.com/gravitational/teleport/lib/services"
+
+	"github.com/gravitational/kingpin"
+	"github.com/gravitational/trace"
+)
+
+// DbCommand implements "tctl db" group of commands.
+type DbCommand struct {
+	config *service.Config
+
+	// format is the output format (text, json, or yaml).
+	format string
+
+	// dbList implements the "tctl db ls" subcommand.
+	dbList *kingpin.CmdClause
+}
+
+// Initialize allows DBCommand to plug itself into the CLI parser.
+func (c *DbCommand) Initialize(app *kingpin.Application, config *service.Config) {
+	c.config = config
+
+	db := app.Command("db", "Operate on databases registered with the cluster.")
+	c.dbList = db.Command("ls", "List all databases registered with the cluster.")
+	c.dbList.Flag("format", "Output format, 'text', 'json', or 'yaml'").Default("text").StringVar(&c.format)
+}
+
+// TryRun attempts to run subcommands like "apps ls".
+func (c *DbCommand) TryRun(cmd string, client auth.ClientI) (match bool, err error) {
+	switch cmd {
+	case c.dbList.FullCommand():
+		err = c.ListDatabases(client)
+	default:
+		return false, nil
+	}
+	return true, trace.Wrap(err)
+}
+
+// ListDatabases prints the list of database proxies that have recently sent
+// heartbeats to the cluster.
+func (c *DbCommand) ListDatabases(client auth.ClientI) error {
+	servers, err := client.GetDatabaseServers(context.TODO(), defaults.Namespace, services.SkipValidation())
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	coll := &dbCollection{servers: servers}
+	switch c.format {
+	case teleport.Text:
+		err = coll.writeText(os.Stdout)
+	case teleport.JSON:
+		err = coll.writeJSON(os.Stdout)
+	case teleport.YAML:
+		err = coll.writeYAML(os.Stdout)
+	default:
+		return trace.BadParameter("unknown format %q", c.format)
+	}
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return nil
+}
+
+const dbMessage = `The invite token: %v.
+This token will expire in %d minutes.
+
+Fill out and run this command on a node to start proxying the database:
+
+> teleport start \
+   --roles=%v \
+   --token=%v \
+   --ca-pin=%v \
+   --auth-server=%v \
+   --db-name=%v \
+   --db-protocol=%v \
+   --db-uri=%v 
+
+Please note:
+
+  - This invitation token will expire in %d minutes.
+  - %v must be reachable from the new database service.
+`